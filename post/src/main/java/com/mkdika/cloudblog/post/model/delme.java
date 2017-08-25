4f9b21d05c8b097a969768de4be7058425f05787
Maikel Chandika <mkdika@gmail.com>
2017-08-25 21:19:17 +0700

    add 2 sub project

---
+++ post/src/main/java/com/mkdika/cloudblog/post/model/delme.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mkdika.cloudblog.post.model;
+
+/**
+ *
+ * @author Maikel Chandika <mkdika@gmail.com>
+ */
+public class delme {
+    
+}
