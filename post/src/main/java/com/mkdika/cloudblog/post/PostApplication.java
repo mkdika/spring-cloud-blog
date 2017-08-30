9de1e74c52984390eb8f9b797c1618cbb2507936
Maikel Chandika <mkdika@gmail.com>
2017-08-30 22:05:33 +0700

    fix and add eureka server

--- post/src/main/java/com/mkdika/cloudblog/post/PostApplication.java
+++ post/src/main/java/com/mkdika/cloudblog/post/PostApplication.java
@@ -2,10 +2,12 @@ package com.mkdika.cloudblog.post;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PostApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(PostApplication.class, args);
 	}
